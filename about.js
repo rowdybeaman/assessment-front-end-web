45985f3b4ce707ff1c22174b4b1794463e57779b
Rowdy Beaman <rowdybeaman@gmail.com>
2024-03-13 14:45:59 -0600

    done

--- about.js
+++ about.js
@@ -1,14 +1,25 @@
 console.log("hello world");
 
-
-
 function handleSubmit(evt) {
-	evt.preventDefault();
-	
-	console.log('form submit');
+    evt.preventDefault();
+    alert('Form submitted successfully!');
 }
 
+function giveCompliment() {
+    alert('You are looking great today!');
+}
 
 let form = document.querySelector('#contact');
+let duckImage = document.querySelector('#duck-image');
 
 form.addEventListener('submit', handleSubmit);
+
+duckImage.addEventListener('mouseover', giveCompliment);
+// can i just say i hate the mouseover event and function alert in a combo together! It should be a click or if its a mouseover it should use a dropdown not an alert lol. anyway thank you for coming to my ted talk
+
+
+
+
+
+
+
---
+++ profile.js
@@ -0,0 +1,15 @@
+document.getElementById("color").addEventListener("click", function() {
+    alert("My favorite color is Yellow!")
+})
+
+document.getElementById("place").addEventListener("click", function() {
+    alert("My favorite place is wherever the food is!")
+})
+
+document.getElementById("ritual").addEventListener("click", function() {
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+})
+//i know what you are thinking... ROWDY THATS THE SECOND RICK ROLL YOU'VE DONE TO ME TODAY! And you'd be right.... that is all
+
+
+
